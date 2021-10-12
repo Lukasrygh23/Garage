d44e6136e7d64072ab1fcc8bf060bc21bb6e2c51
LukasR23 <Lukas@jazzcatz.com>
2021-10-12 13:41:59 +0100

    Not sure what's in this one, but I just had lunch

---
+++ src/com/qa/vehicle/Garage.java
@@ -0,0 +1,16 @@
+package com.qa.vehicle;
+
+import java.util.ArrayList;
+
+public class Garage {
+	ArrayList<Vehicle> Garage = new ArrayList<>();
+	
+	public void addVehicle(Vehicle item) {
+		Garage.add(item);
+	}
+	
+	
+	public void emptyGarage() {
+		Garade.void();
+	}
+}
